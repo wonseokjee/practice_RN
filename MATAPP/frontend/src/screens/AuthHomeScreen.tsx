4a7e9cb740d0edbe669dcd64faa83bd13c4b95cc
wonseokjee <wsji9404@gmail.com>
2025-04-15 21:26:10 +0900

    [setting] navigation, screen setting

---
+++ MATAPP/frontend/src/screens/AuthHomeScreen.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+// eslint-disable-next-line prettier/prettier
+
+function AuthHomeScreen() {
+  return (
+    <View>
+      <Button title="로그인 화면으로 이동" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default AuthHomeScreen;
